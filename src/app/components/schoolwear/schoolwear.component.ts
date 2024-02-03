33a4e4e23f71a93782ddb3271e30c98b4b0f7337
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2024-02-03 13:31:36 +0100

    $*

--- src/app/components/schoolwear/schoolwear.component.ts
+++ src/app/components/schoolwear/schoolwear.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SliderImage } from 'src/app/shared/image-interface';
 
 @Component({
   selector: 'app-schoolwear',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./schoolwear.component.scss']
 })
 export class SchoolwearComponent {
-
+  sliderImages: SliderImage[] = [
+    {
+      src: 'assets/img/phone_1.jpg',
+      text: 'Sehr schönes Stück',
+      link: '#'
+    },
+    {
+      src: 'assets/img/phone_2.jpg',
+      text: 'Aus unserer Sommer-Kollektion',
+      link: '#'
+    },
+    {
+      src: 'assets/img/phone_3.jpg',
+      text: 'Gerade im Super-Spar-Angebot',
+      link: '#'
+    },
+    {
+      src: 'assets/img/phone_4.jpg',
+      text: 'Der letzte Schrei',
+      link: '#'
+    },
+    {
+      src: 'assets/img/slider_img1.jpg',
+      text: 'Zum Sonderangebot',
+      link: '#'
+    },
+    {
+      src: 'assets/img/slider_img2.jpg',
+      text: 'Zum Sonderangebot',
+      link: '#'
+    },
+    {
+      src: 'assets/img/slider_img3.jpg',
+      text: 'Zum Sonderangebot',
+      link: '#'
+    },
+    {
+      src: 'assets/img/slider_img4.jpg',
+      text: 'Zum Sonderangebot',
+      link: '#'
+    },
+  ]
 }
