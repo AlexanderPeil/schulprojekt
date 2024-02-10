c9c38981fca468f53d2a9e0b26728b943b4590ff
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2024-02-10 22:30:49 +0100

    $*

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SliderImage } from 'src/app/shared/image-interface';
 
 @Component({
   selector: 'app-courses',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
+  
+  courses: SliderImage[] = [
+    {
+      src: 'assets/img/phone_1.jpg',
+      headline: 'Smartphone Kurs 1',
+      text: 'Das war unser letzter Kurs',
+      link: '#'
+    },
+    {
+      src: 'assets/img/phone_2.jpg',
+      headline: 'Smartphone Kurs 2',
+      text: 'Wir haben neue Kurse am Start',
+      link: '#'
+    },
+    {
+      src: 'assets/img/phone_3.jpg',
+      headline: 'Smartphone Kurs 3',
+      text: 'Hier können sie unsere Zeiten einsehen',
+      link: '#'
+    },
+    {
+      src: 'assets/img/phone_4.jpg',
+      headline: 'Smartphone Kurs 4',
+      text: 'Das planen wir in der Zukunft',
+      link: '#'
+    }
+  ];
 
+  scrollToTop(event: Event) {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  
 }
--- src/app/shared/image-interface.ts
+++ src/app/shared/image-interface.ts
@@ -1,5 +1,6 @@
 export interface SliderImage {
     src: string;
+    headline?: string;
     text: string;
     link: string;
     price?: string;
